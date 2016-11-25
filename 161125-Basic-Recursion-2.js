6f30ae601bf0dc91bdbfd1484ee21978a47b7966
Surya Kotamraju <kotamraju@me.com>
2016-11-25 00:31:04 -0500

    rename recursion to include parts, add closure concept

---
+++ 161125-Basic-Recursion-2.js
@@ -0,0 +1,16 @@
+// write a recursive function that takes in an array and uses recursion
+// to return its contents in reverse
+
+// closure practice
+function a() {
+  var apple = 'appl';
+  b();
+
+  console.log(apple);
+}
+
+function b() {
+  apple = 'apple';
+}
+
+a();
