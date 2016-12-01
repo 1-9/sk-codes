6f6ed8dd72118cce5769472a00705e313d1f8903
Surya Kotamraju <kotamraju@me.com>
2016-12-01 15:04:05 -0500

    bubble sort

--- 161130-Bubble-Sort.js
+++ 161130-Bubble-Sort.js
@@ -1,8 +1,16 @@
+// Naive bubble sort implementation
 function bubbleSort(arr) {
-  let trySorting = true;
-  while (trySorting) {
-
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
   }
   return arr;
 };
-bubbleSort([2, 38, 9, 1, 4, 5]);
+
+bubbleSort([2, 3, 38, 9, 1, 4, 5]);
+bubbleSort([2, -3, 7, 9, -10, 0,]);
