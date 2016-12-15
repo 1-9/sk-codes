eb8d3dd9bf6354b2f0df0f17c738d38205aa8459
Surya Kotamraju <kotamraju@me.com>
2016-12-14 22:06:49 -0500

    refactor console

--- 161212-Object-Prototype.js
+++ 161212-Object-Prototype.js
@@ -1,28 +1,31 @@
+let log = console.log;
+let dir = console.dir;
+
 let myObjLiteral = {
   ny: 'New York'
 };
 
 let myObjConstructor = new Object();
 
-console.log(7, '161212-Object-Prototype.js', myObjLiteral instanceof Object); // true
-console.log(8, '161212-Object-Prototype.js', myObjConstructor instanceof Object); // true
-console.dir(myObjConstructor, {
+log(7, '161212-Object-Prototype.js', myObjLiteral instanceof Object); // true
+log(8, '161212-Object-Prototype.js', myObjConstructor instanceof Object); // true
+dir(myObjConstructor, {
   depth: null
 });
-console.dir(myObjLiteral, {
+dir(myObjLiteral, {
   depth: null
 });
 
 let literalStr = 'California';
 let constructorStr = new String('Alabama');
 
-console.log(15, '161212-Object-Prototype.js', literalStr instanceof String); // false
-console.log(16, '161212-Object-Prototype.js', constructorStr instanceof String); // true
+log(15, '161212-Object-Prototype.js', literalStr instanceof String); // false
+log(16, '161212-Object-Prototype.js', constructorStr instanceof String); // true
 
-console.log(18, '161212-Object-Prototype.js', typeof literalStr); // string
-console.log(19, '161212-Object-Prototype.js', typeof constructorStr); // object
+log(18, '161212-Object-Prototype.js', typeof literalStr); // string
+log(19, '161212-Object-Prototype.js', typeof constructorStr); // object
 
-console.dir(myObjConstructor, {
+dir(myObjConstructor, {
   depth: null
 });
 
@@ -37,8 +40,8 @@ Object.defineProperty(obj2, "a", {
   enumerable: true,
 });
 
-console.dir(obj1, { depth: null });
-console.dir(obj2, { depth: null });
+dir(obj1, { depth: null });
+dir(obj2, { depth: null });
 
 
 let obj3 = {
@@ -56,11 +59,11 @@ Object.defineProperty(
   }
 );
 
-console.dir(obj3, { depth: null });
-console.dir(obj3.a, { depth: null });
-console.dir(obj3.b, { depth: null });
+dir(obj3, { depth: null });
+dir(obj3.a, { depth: null });
+dir(obj3.b, { depth: null });
 
 let a1 = [1, 2, 3];
 for (let i in a1) {
-  console.log(65, '161212-Object-Prototype.js', a1[i]);
+  log(65, '161212-Object-Prototype.js', a1[i]);
 }
