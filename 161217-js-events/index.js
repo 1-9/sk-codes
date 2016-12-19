ee76a6caeedfa629fbf1f0ae079bb065d724432b
Surya Kotamraju <kotamraju@me.com>
2016-12-19 18:01:41 -0500

    events in javascript

--- 161217-js-events/index.js
+++ 161217-js-events/index.js
@@ -1,23 +1,22 @@
 window.onload = function () {
-  init();
-};
 
-function init() {
+  let container2 = document.getElementById('container2');
+  container2.addEventListener('click', container2Event, true);
+
+  function container2Event(event) {
+    console.log(13, 'container2.js', event);
+  }
+
 
   let container1 = document.getElementById('container1');
   container1.addEventListener('click', function (event) {
+    let time = Date.now();
+    let newTime = time + 10000;
+    while (newTime >= Date.now()) {}
     console.log(8, 'container1.js', event);
   }, false);
 
-  let container2 = document.getElementById('container2');
-  container2.addEventListener('click', function (event) {
-    console.log(13, 'container2.js', event);
-    event.stopPropagation();
-  }, false);
+  container2.click();
+  container1.click();
 
-  let mainContainer = document.getElementById('mainContainer');
-  mainContainer.addEventListener('click', function (event) {
-    console.log(18, 'maincontainer.js', event);
-  }, true);
-
-}
+};
