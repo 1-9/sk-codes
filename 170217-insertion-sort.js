6e9b4fd36de94ceb7c506a8d96d96e76791f9707
Surya Kotamraju <kotamraju@me.com>
2017-02-19 23:01:03 -0500

    insertion sort

--- 170217-insertion-sort.js
+++ 170217-insertion-sort.js
@@ -1,9 +1,19 @@
 function insertionSort(arr) {
-  for (var i = 0; i < arr.length; i++) {
+  for (var i = 1; i < arr.length; i++) {
+    var currentItem = arr[i];
     for (var j = 0; j < i; j++) {
-      if (arr[i] < arr[j]) {
-
+      if (currentItem < arr[j]) {
+        for (var k = i; k <= j; k--) {
+          arr[k] = arr[k - 1];
+        }
+        arr[j] = currentItem;
       }
     }
   }
+  return arr;
 }
+
+// test selectionSortNew function
+console.log(25, '170216-insertion-sort.js', insertionSort([8, 3, 5, 1, 9])); // [ 1, 3, 5, 8, 9 ]
+console.log(25, '170216-insertion-sort.js', insertionSort([8, 3, -5, -4, 1, 9])); //[ -5, -4, 1, 3, 8, 9 ]
+console.log(25, '170216-insertion-sort.js', insertionSort([-8, -3, -5, -0, 1, 9])); //[ -8, -5, -3, -0, 1, 9 ]
