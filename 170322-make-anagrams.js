3b0a82f8d68b6121dcfb74c3973dee0acdda98de
Surya Kotamraju <kotamraju@me.com>
2017-03-22 14:24:52 -0400

    update logic with two for loops

--- 170322-make-anagrams.js
+++ 170322-make-anagrams.js
@@ -1,8 +1,31 @@
+//////////////////////////////////////////
+// let sortedA = a.split('').sort(),    //
+//   sortedB = a.split('').sort(),      //
+// let ans = 0;                         //
+//                                      //
+// for (var i = 0; i < a.length; i++) { //
+//   if (b.indexOf(a[i]) === -1) {      //
+//     ans++;                           //
+//   }                                  //
+// }                                    //
+//                                      //
+// for (var j = 0; j < b.length; j++) { //
+//   if (a.indexOf(b[j]) === -1) {      //
+//     ans++;                           //
+//   }                                  //
+// }                                    //
+// return ans;                          //
+//////////////////////////////////////////
+
 function anagram(a, b) {
   let ans = 0,
     common = 0,
     duplicateFinder = a.split('').reduce(function (acc, val) {
-      acc[val] = val;
+      if (acc[val]) {
+        common--;
+      } else {
+        acc[val] = val;
+      }
       return acc;
     }, {});
   b.split('').forEach(function (val) {
@@ -15,6 +38,6 @@ function anagram(a, b) {
   return a.length - common + ans;
 }
 
-console.log(23, '170322-make-anagrams.js', anagram('abcd', 'cef'));
-console.log(25, '170322-make-anagrams.js', anagram('cde', 'abc'));
-console.log(20, '170322-make-anagrams.js', anagram('fcrxzwscanmligyxyvym', 'jxwtrhvujlmrpdoqbisbwhmgpmeoke'));
+console.log(41, '170322-make-anagrams.js', anagram('abcd', 'cef'));
+console.log(42, '170322-make-anagrams.js', anagram('cde', 'abc'));
+console.log(43, '170322-make-anagrams.js', anagram('fcrxzwscanmligyxyvym', 'jxwtrhvujlmrpdoqbisbwhmgpmeoke'));
