a3a7b62b692d8b0e80eb609a0d9e7f5b860235dc
Surya Kotamraju <kotamraju@me.com>
2017-04-08 12:22:33 -0400

    custom reduce

--- 170407-reduce.js
+++ 170407-reduce.js
@@ -1,13 +1,24 @@
-// var sum = [0, 1, 2, 3].reduce(function(acc, val) {
-//   return acc + val;
-// }, 0);
-
 Array.prototype.skreduce = function skreduce(cb) {
-  return function () {
-    return this.map(function () {
-
-    });
+  if (this.length === 1) {
+    return this[0];
   };
-}
+  var ans = this[0];
+  for (var i = 1; i < this.length; i++) {
+    ans = cb(ans, this[i]);
+  }
+  return ans;
+};
 
-[1, 2, 3].skreduce(add);
+let add = (x, y) => x * y;
+
+let a = [1, 8, 3].skreduce(add);
+console.log(a);
+
+let b = [1, 8, 3].reduce(add);
+console.log(b);
+
+let c = [8].skreduce(add);
+console.log(c);
+
+let d = [8].reduce(add);
+console.log(d);
