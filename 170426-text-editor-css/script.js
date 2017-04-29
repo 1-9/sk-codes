307aeaa5cc2d1947680c85f841ce855384363302
Surya Kotamraju <kotamraju@me.com>
2017-04-29 12:15:23 -0400

    arrow functions

--- 170426-text-editor-css/script.js
+++ 170426-text-editor-css/script.js
@@ -4,4 +4,25 @@ window.onload = function () {
   console.log(body.innerText);
   body.innerText = Array.from(document.querySelectorAll('.main-container'))
     .map(() => `simple data`).join(' ');
+
+  function hello() {
+    console.log('hello');
+  }
+
+  hello();
+
+  let beta = (x) => {
+    if (x > 0) {
+      return beta(--x);
+    }
+    return x;
+  };
+
+  beta(10);
+
+  function good(...a) {
+    console.log(a);
+  }
+  good(1, 2, 3);
+
 };
