502a9e8178f83d13c48101425852bb2c28c3bb0f
Surya Kotamraju <kotamraju@me.com>
2017-05-05 23:37:39 -0400

    add c basics initial commit

---
+++ 170505-basics.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("hello worlds\n");
+  int y = 9;
+  short a = 9;
+  int b = 2 * y + 2 * a;
+  printf("%d\n", y);
+  printf("%d\n", a);
+  printf("%d\n", b);
+  return 0;
+}
