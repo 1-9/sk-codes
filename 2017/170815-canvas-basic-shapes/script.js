dfae7d4ed20eb4d3c6c270eb99fb3a867525a88b
Surya Kotamraju <kotamraju@me.com>
2017-09-03 00:40:10 -0400

    finish canvas ping pong game, add flex-box layout practice

--- 2017/170815-canvas-basic-shapes/script.js
+++ 2017/170815-canvas-basic-shapes/script.js
@@ -1,69 +1,126 @@
 window.onload = function () {
-  let canvas = document.querySelector("canvas"),
+  let canvas = document.createElement('canvas'),
+    scoreElement = document.querySelector('.score'),
     ctx = canvas.getContext("2d"),
-    balls = [];
+    ballR = 10,
+    balls = [],
+    paddles = [],
+    xDirections = ['left', 'right'],
+    yDirections = ['up', 'down'],
+    score = 0,
+    playGame;
 
-  ctx.canvas.width = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
+  document.body.appendChild(canvas)
+  ctx.canvas.width = window.innerWidth
+  ctx.canvas.height = window.innerHeight
 
-  drawAll();
+  drawAll()
+
+  createBall(5)
+  createPaddle()
 
   function drawAll() {
-    drawBackground();
-    if (balls.length < 10) {
-      createBall();
-    }
-    drawBall();
-    window.requestAnimationFrame(drawAll);
+    let fps = 30
+    drawBackground()
+    drawBall()
+    drawPaddle()
+
+    setTimeout(() => {
+      playGame = window.requestAnimationFrame(drawAll)
+    }, 1000 / fps)
   }
 
-  function createBall() {
-    balls.push({
-      ballX: getRandomArbitrary(0, ctx.canvas.width),
-      ballY: getRandomArbitrary(0, ctx.canvas.height),
-      yDirection: "down",
-      xDirection: "right",
-      speed: getRandomArbitrary(1, 10)
+  function randomDirection(coordinate) {
+    return Math.random() < 0.5 ? coordinate === 'x' ? 'left' : 'up' : coordinate === 'x' ? 'right' : 'down'
+  }
+
+  function createBall(total) {
+    for (let i = 0; i < total; i++) {
+      balls.push({
+        ballX: getRandomArbitrary(0, ctx.canvas.width),
+        ballY: getRandomArbitrary(0, ctx.canvas.height),
+        yDirection: randomDirection('y'),
+        xDirection: randomDirection('x'),
+        speed: getRandomArbitrary(0, 5)
+      })
+    }
+  }
+
+  function createPaddle() {
+    paddles.push({
+      paddleWidth: 20,
+      paddleHeight: ctx.canvas.height / 8,
+      paddleX: 0,
+      paddleY: getRandomArbitrary(0, ctx.canvas.height - 100),
+      speed: ctx.canvas.height / 8 * 0.5
     })
   }
 
   function drawBall() {
-    balls.forEach(function (ball) {
-      ballXDirection(ball);
-      ballYDirection(ball);
-      ball.xDirection === "left" ? (ball.ballX -= ball.speed) : (ball.ballX += ball.speed);
-      ball.yDirection === "down" ? (ball.ballY += ball.speed) : (ball.ballY -= ball.speed);
-
-      ctx.beginPath();
-      ctx.fillStyle = "green";
-      ctx.arc(ball.ballX, ball.ballY, 10, 0, 2 * Math.PI, true);
-      ctx.fill();
+    balls.forEach(function (ball, index) {
+      ballXDirection(ball, index)
+      ballYDirection(ball)
+      ball.speed += 0.005
+      ball.xDirection === "left" ? (ball.ballX -= ball.speed) : (ball.ballX += ball.speed)
+      ball.yDirection === "down" ? (ball.ballY += ball.speed) : (ball.ballY -= ball.speed)
+      ctx.beginPath()
+      ctx.fillStyle = "green"
+      ctx.arc(ball.ballX, ball.ballY, ballR, 0, 2 * Math.PI, true)
+      ctx.fill()
     })
   }
 
-  function ballXDirection(ball) {
-    // if (ball.ballX > ctx.canvas.width - 10) {
-    //   ball.xDirection = "left";
-    // } else if (ball.ballX < 10) {
-    //   ball.xDirection = "right";
-    // }
+  document.addEventListener("keydown", function (event) {
+    let paddle = paddles[0]
+    if (paddle.paddleY > 0 && event.which === 75) {
+      paddle.paddleY -= paddle.speed
+    } else if (paddle.paddleY < ctx.canvas.height - paddle.paddleHeight && event.which === 74) {
+      paddle.paddleY += paddle.speed
+    }
+  })
+
+  function drawPaddle() {
+    paddles.forEach(function (paddle) {
+      ctx.beginPath()
+      ctx.fillStyle = "indianred"
+      ctx.rect(paddle.paddleX, paddle.paddleY, paddle.paddleWidth, paddle.paddleHeight)
+      ctx.fill()
+    })
   }
 
+  function ballXDirection(ball, index) {
+    let paddle = paddles[0]
+    if (ball.ballX > ctx.canvas.width - ballR) {
+      ball.xDirection = "left"
+    } else if (ball.ballX < paddle.paddleWidth + ballR && ball.ballX > 0 &&
+      paddle.paddleY < ball.ballY &&
+      paddle.paddleY + paddle.paddleHeight > ball.ballY) {
+      score += Math.round(ball.speed * 2);
+      scoreElement.innerText = score;
+      ball.xDirection = "right"
+    } else if (ball.ballX < 0) {
+      balls.splice(index, 1);
+      if (!balls.length) {
+        paddles = [];
+        window.cancelAnimationFrame(playGame);
+      }
+    }
+  }
 
   function ballYDirection(ball) {
-    if (ball.ballY > ctx.canvas.height - 10) {
-      ball.yDirection = "up";
-    } else if (ball.ballY < 10) {
-      ball.yDirection = "down";
+    if (ball.ballY > ctx.canvas.height - ballR) {
+      ball.yDirection = "up"
+    } else if (ball.ballY < ballR) {
+      ball.yDirection = "down"
     }
   }
 
   function drawBackground() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+    ctx.fillStyle = "black"
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
   }
 
   function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+    return Math.random() * (max - min) + min
   }
-};
+}
