dab6f6e469df33bb31aa00622b8dc2c15e2e495b
Surya Kotamraju <kotamraju@me.com>
2017-10-29 23:15:32 -0400

    add crossword puzzle, regular expression match, rename base, login page demo

---
+++ 2017/171024-crossword-puzzle.js
@@ -0,0 +1,20 @@
+let input = [
+    "+-++++++++",
+    "+-++++++++",
+    "+-++++++++",
+    "+-----++++",
+    "+-+++-++++",
+    "+-+++-++++",
+    "+++++-++++",
+    "++------++",
+    "+++++-++++",
+    "+++++-++++"
+  ],
+  options = ["LONDON", "DELHI", "ICELAND", 'ANKARA'];
+
+function processData(input, options) {
+  //Enter your code here
+  console.log(input, options);
+}
+
+processData(input, options);
