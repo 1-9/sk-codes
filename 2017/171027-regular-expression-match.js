3676b6073f90102257dbd8bc289efc3164688839
Surya Kotamraju <kotamraju@me.com>
2017-11-16 20:24:39 -0500

    regular expression matching

--- 2017/171027-regular-expression-match.js
+++ 2017/171027-regular-expression-match.js
@@ -2,11 +2,10 @@ var isMatch = function(s, p) {
   console.log(s, p);
 };
 
-isMatch("aa","a")
-isMatch("aa","aa")
-isMatch("aaa","aa")
-isMatch("aa", "a*")
-isMatch("aa", ".*")
-isMatch("ab", ".*")
-isMatch("aab", "c*a*b")
-
+isMatch("aa", "a");
+isMatch("aa", "aa");
+isMatch("aaa", "aa");
+isMatch("aa", "a*");
+isMatch("aa", ".*");
+isMatch("ab", ".*");
+isMatch("aab", "c*a*b");
