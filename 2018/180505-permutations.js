a5a534077b10eb0bb321cb575e50ab741032440e
Surya Kotamraju <kotamraju@me.com>
2018-05-07 23:36:50 -0400

    rename integer to binary

--- 2018/180505-permutations.js
+++ 2018/180505-permutations.js
@@ -1,17 +1,15 @@
-function permutations(str, chosen = '', ans = []) {
+function permutations(str, currentStr = '') {
   if (str === '') {
-    ans.push(chosen);
+    console.log(currentStr);
   } else {
     for (let i = 0; i < str.length; i++) {
-      chosen += str[i];
+      currentStr += str[i];
       str = str.slice(0, i) + str.slice(i + 1);
-      permutations(str, chosen, ans);
-
-      str = str.slice(0, i) + chosen[chosen.length - 1] + str.slice(i);
-      chosen = chosen.slice(0, chosen.length - 1);
+      permutations(str, currentStr);
+      str = str.slice(0, i) + currentStr[currentStr.length - 1] + str.slice(i);
+      currentStr = currentStr.slice(0, currentStr.length - 1);
     }
   }
-  return ans;
 }
 
-console.log(21, permutations('abc'));
+permutations('abcd');
