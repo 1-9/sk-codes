9f5c57f8ba5d9cc46d3980a9c12065073fba4672
Surya Kotamraju <kotamraju@me.com>
2018-05-09 21:14:26 -0400

    update three sum

--- 2018/180506-three-sum.js
+++ 2018/180506-three-sum.js
@@ -1,13 +1,8 @@
 // Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
-
 // Note:
-
 // The solution set must not contain duplicate triplets.
-
 // Example:
-
 // Given array nums = [-1, 0, 1, 2, -1, -4],
-
 // A solution set is:
 // [
 //   [-1, 0, 1],
@@ -18,42 +13,20 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-
-var threeSum = function(nums) {
-  let sortedNums = nums.slice().sort((a, b) => a - b);
-  min = sortedNums.shift();
-  max = sortedNums.pop();
-  return threeSumHelper(nums, min, max);
-};
-
-function threeSumHelper(nums, min, max, soFar = [], ans = [], hash = {}) {
-  if (soFar.length === 2) {
-    for (let i = 0; i < nums.length; i++) {
-      let current = nums[i];
-      let currentTotal = soFar.reduce((a, b) => a + b, current);
-
-      if (currentTotal === 0) {
-        let possibleAns = [...soFar, current];
-        possibleAns = possibleAns.sort((a, b) => a - b);
-        if (!hash[possibleAns]) {
-          hash[possibleAns] = possibleAns;
-          ans.push(possibleAns);
-        }
-      }
+function threeSum(nums, current = [], ans = [], level = 0, hash = {}) {
+  if (current.length === 3) {
+    let sortCurrent = current.sort((a, b) => a - b);
+    let currentTotal = current.reduce((a, b) => a + b, 0);
+    if (!currentTotal && !hash[sortCurrent]) {
+      ans.push(sortCurrent);
+      hash[sortCurrent] = true;
     }
   } else {
-    for (let i = 0; i < nums.length; i++) {
-      let current = nums[i];
-      callRecursion(current, i);
+    for (let i = level; i < nums.length; i++) {
+      current.push(nums[i]);
+      threeSum(nums, [...current], ans, ++level, hash);
+      current.pop();
     }
-  }
-
-  function callRecursion(current, i) {
-    soFar.push(current);
-    nums.splice(i, 1);
-    threeSumHelper(nums, min, max, [...soFar], ans, hash);
-    nums.splice(i, 0, current);
-    soFar.pop();
   }
   return ans;
 }
