f08d2ddb9f88af526230ddb4118d20558a50dd0f
Surya Kotamraju <kotamraju@me.com>
2018-05-13 18:30:33 -0400

    update matching paranthesis

--- 2016/170317-valid-paranthesis.js
+++ 2016/170317-valid-paranthesis.js
@@ -1,30 +1,20 @@
-/////////////////////////////////////////////////////////////////////////////////////////////////
-// Write a function called validParentheses that takes a string of parentheses, and determines //
-// if the order of the parentheses is valid. validParentheses                                  //
-// should return true if the string is valid, and false if it's invalid.                       //
-//                                                                                             //
-// Examples:                                                                                   //
-// validParentheses( "()" ) => returns true                                                    //
-// validParentheses( ")(()))" ) => returns false                                               //
-// validParentheses( "(" ) => returns false                                                    //
-// validParentheses( "(())((()())())" ) => returns true                                        //
-//                                                                                             //
-// All input strings will be nonempty, and will only consist of open                           //
-// parentheses '(' and/or closed parentheses ')'                                               //
-/////////////////////////////////////////////////////////////////////////////////////////////////
+const matches = {
+  '[': ']',
+  '(': ')',
+  '{': '}'
+};
 
-function validParentheses(parens) {
-  let parensStack = [parens[0]];
-  for (var i = 1; i < parens.length; i++) {
-    if (parensStack[parensStack.length - 1] === '(' && parens[i] === ')') {
-      parensStack.pop();
+function isValid(s) {
+  return !s.split('').reduce(function(acc, val, ind) {
+    if (matches[acc[acc.length - 1]] === val) {
+      acc.pop();
     } else {
-      parensStack.push(parens[i]);
+      acc.push(val);
     }
-  }
-  return !parensStack.length;
+    return acc;
+  }, []).length;
 }
 
-console.log(36, '170317-valid-paranthesis.js', validParentheses('()')); // true
-console.log(36, '170317-valid-paranthesis.js', validParentheses('())(')); // false
-console.log(36, '170317-valid-paranthesis.js', validParentheses('(())())))')); // false
+console.log(isValid('()[]{}'));
+console.log(isValid('()'));
+console.log(isValid('()'));
