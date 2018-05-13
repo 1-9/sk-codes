9b1f8df7c637aa77e5b2bc4b9e28264351c2342b
Surya Kotamraju <kotamraju@me.com>
2018-05-13 08:45:35 -0400

    update permutations, add powersets

--- 2018/180505-permutations.js
+++ 2018/180505-permutations.js
@@ -1,15 +1,35 @@
-function permutations(str, currentStr = '') {
-  if (str === '') {
-    console.log(currentStr);
+function permutations(str, currentStr = '', ans = []) {
+  if (!str.length) {
+    ans.push(currentStr);
   } else {
     for (let i = 0; i < str.length; i++) {
+      let current = str[i];
       currentStr += str[i];
       str = str.slice(0, i) + str.slice(i + 1);
-      permutations(str, currentStr);
-      str = str.slice(0, i) + currentStr[currentStr.length - 1] + str.slice(i);
+      permutations(str, currentStr, ans);
       currentStr = currentStr.slice(0, currentStr.length - 1);
+      str = str.slice(0, i) + current + str.slice(i);
     }
   }
+  console.log(ans);
+  return ans;
 }
 
-permutations('abcd');
+function subsets(str, currentStr = '', ans = []) {
+  if (!str.length) {
+    ans.push(currentStr);
+  } else {
+    let current = str[0];
+    str = str.slice(1);
+    subsets(str, currentStr + current, ans);
+    subsets(str, currentStr, ans);
+    str = current + str;
+  }
+  return ans;
+}
+
+console.log(permutations('abc'));
+console.log(permutations('abc').length);
+console.log(subsets('abcd'));
+console.log(permutations('abcd'));
+console.log(permutations('abcd').length);
