a85658961935ec0291b41f84930ba8dfb2779bec
Surya Kotamraju <kotamraju@me.com>
2018-05-29 22:18:00 -0400

    update word search

--- 2018/180625-word-search.js
+++ 2018/180625-word-search.js
@@ -15,12 +15,11 @@
 // Given word = "SEE", return true.
 // Given word = "ABCB", return false.
 
-var exist = function(board, word) {
-  let ans = false;
+var exist = function(board, word, ans = false) {
   loop1: for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      if (board[i][j] === word[0] && !ans) {
-        ans = helper(board, i, j, word, 1, { [i + '' + j]: true });
+      if (!ans && board[i][j] === word[0]) {
+        ans = helper(board, word, i, j);
       }
       if (ans) {
         break loop1;
@@ -32,42 +31,38 @@ var exist = function(board, word) {
   return ans;
 };
 
-function helper(board, i, j, word, ind, hash) {
-  if (ind === word.length) {
-    return true;
-  } else if (
-    board[i + 1] &&
-    board[i + 1][j] &&
-    board[i + 1][j] === word[ind] &&
-    !hash[i + 1 + '' + j]
-  ) {
-    hash[i + 1 + '' + j] = true;
-    return helper(board, i + 1, j, word, ++ind, hash);
+function helper(board, word, row, column, ind = 0, ans = [false]) {
+  if (board[row] && board[row][column] === word[ind]) {
+    if (ind === word.length - 1) {
+      ans[0] = true;
+    } else {
+      let current = board[row][column];
+      board[row][column] = null;
+      if (board[row][column - 1] === word[ind + 1]) {
+        helper(board, word, row, column - 1, ind + 1, ans);
+      }
+      if (board[row][column + 1] === word[ind + 1]) {
+        helper(board, word, row, column + 1, ind + 1, ans);
+      }
+      if (board[row + 1] && board[row + 1][column] === word[ind + 1]) {
+        helper(board, word, row + 1, column, ind + 1, ans);
+      }
+      if (board[row - 1] && board[row - 1][column] === word[ind + 1]) {
+        helper(board, word, row - 1, column, ind + 1, ans);
+      }
+      board[row][column] = current;
+    }
   }
-  if (
-    board[i - 1] &&
-    board[i - 1][j] &&
-    board[i - 1][j] === word[ind] &&
-    !hash[i - 1 + '' + j]
-  ) {
-    hash[i - 1 + '' + j] = true;
-    return helper(board, i - 1, j, word, ++ind, hash);
-  }
-  if (board[i][j + 1] === word[ind] && !hash[i + '' + (j + 1)]) {
-    hash[i + '' + (j + 1)] = true;
-    return helper(board, i, j + 1, word, ++ind, hash);
-  }
-  if (board[i][j - 1] === word[ind] && !hash[i + '' + (j - 1)]) {
-    hash[i + '' + (j - 1)] = true;
-    return helper(board, i, j - 1, word, ++ind, hash);
-  }
-  return false;
+  return ans[0];
 }
 
-var board = [['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']];
+var board1 = [['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']];
 var board2 = [['a', 'a']];
 var board3 = [['C', 'A', 'A'], ['A', 'A', 'A'], ['B', 'C', 'D']];
+var board4 = [['a']];
 
-exist(board, 'ABCCED');
-exist(board2, 'aaa');
-exist(board3, 'AAB');
+exist(board1, 'ABCCED'); // true
+exist(board1, 'ABCCEJ'); // false
+exist(board2, 'aaa'); // false
+exist(board4, 'a'); // true
+exist(board3, 'AAB'); // true
