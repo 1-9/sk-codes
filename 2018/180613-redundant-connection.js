ac77dc00fe587e3bf8fc5ab49901e64024651f07
Surya Kotamraju <kotamraju@me.com>
2018-06-14 18:07:35 -0400

    find redundant node using disjoint union sets

--- 2018/180613-redundant-connection.js
+++ 2018/180613-redundant-connection.js
@@ -2,7 +2,68 @@
  * @param {number[][]} edges
  * @return {number[]}
  */
-var findRedundantConnection = function(edges) {};
+
+var findRedundantConnection = function([edge, ...edges]) {
+  let allSets = [new Set(edge)],
+    ans;
+  for ([u, v] of edges) {
+    let p1, p2;
+    for (let i = 0; i < allSets.length; i++) {
+      if (!p1 && allSets[i].has(u)) {
+        p1 = i;
+      }
+      if (!p2 && allSets[i].has(v)) {
+        p2 = i;
+      }
+    }
+    if (p1 > -1 && p2 > -1 && p1 === p2) {
+      ans = [u, v];
+    }
+    if (p1 > -1 && p2 > -1) {
+      let unionSet = union(allSets[p1], allSets[p2]);
+      allSets[p1] = new Set();
+      allSets[p2] = new Set();
+      allSets.push(unionSet);
+    } else if (p1 > -1) {
+      let unionSet = union(allSets[p1], new Set([u, v]));
+      allSets[p1] = new Set();
+      allSets.push(unionSet);
+    } else if (p2 > -1) {
+      let unionSet = union(allSets[p2], new Set([u, v]));
+      allSets[p2] = new Set();
+      allSets.push(unionSet);
+    }
+
+    if (p1 === undefined && p2 === undefined) {
+      allSets.push(new Set([u, v]));
+    }
+  }
+  console.log(ans);
+  return ans;
+};
+
+function union(setA, setB) {
+  var _union = setA;
+  for (var elem of setB) {
+    _union.add(elem);
+  }
+  return _union;
+}
+
+findRedundantConnection([[1, 2], [1, 3], [2, 3]]);
+// [ 2, 3 ]
+
+findRedundantConnection([[1, 5], [3, 4], [3, 5], [4, 5], [2, 4]]);
+// [ 4, 5 ]
+
+findRedundantConnection([[1, 2], [2, 3], [3, 4], [1, 4], [1, 5]]);
+// [ 1, 4 ]
+
+findRedundantConnection([[1, 3], [3, 4], [1, 5], [3, 5], [2, 3]]);
+// [ 3, 5 ]
+
+findRedundantConnection([[1, 4], [3, 4], [1, 3], [1, 2], [4, 5]]);
+// [ 1, 3 ]
 
 findRedundantConnection([
   [9, 10],
@@ -16,7 +77,4 @@ findRedundantConnection([
   [6, 8],
   [7, 9]
 ]);
-findRedundantConnection([[1, 2], [1, 3], [2, 3]]);
-findRedundantConnection([[1, 2], [2, 3], [3, 4], [1, 4], [1, 5]]);
-findRedundantConnection([[1, 3], [3, 4], [1, 5], [3, 5], [2, 3]]);
-findRedundantConnection([[1, 4], [3, 4], [1, 3], [1, 2], [4, 5]]);
+// [ 4, 10 ]
