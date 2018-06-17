7f1553d6c4d1078519ea60f2f2d7b44f7027ca43
Surya Kotamraju <kotamraju@me.com>
2018-06-17 02:40:09 -0400

    update course schedule

--- 2018/180615-course-schedule.js
+++ 2018/180615-course-schedule.js
@@ -4,55 +4,42 @@
  * @return {boolean}
  */
 var canFinish = function(numCourses, prerequisites) {
-  let allSets = [],
-    ans = true;
+  let ans = [true];
+  let allCourses = {};
   for (let [u, v] of prerequisites) {
-    let p1, p2;
-    if (allSets.length) {
-      for (let i = 0; i < allSets.length; i++) {
-        if (!p1 && allSets[i].has(u)) {
-          p1 = i;
-        }
-        if (!p2 && allSets[i].has(v)) {
-          p2 = i;
-        }
-
-        if (p1 > -1 && p2 > -1) {
-          let newSet = union(allSets[p1], allSets[p2]);
-          allSets.splice(p1, 1);
-          allSets.splice(p2 - 1, 1);
-          ans = false;
-          allSets.push(newSet);
-        } else if (p1 > -1) {
-          let newSet = union(allSets[p1], new Set([u, v]));
-          allSets.splice(p1, 1);
-          allSets.push(newSet);
-        } else if (p2 > -1) {
-          let newSet = union(allSets[p2], new Set([u, v]));
-          allSets.splice(p2, 1);
-          allSets.push(newSet);
-        }
-      }
+    if (!allCourses[u]) {
+      allCourses[u] = [v];
     } else {
-      allSets.push(new Set([u, v]));
+      allCourses[u].push(v);
     }
   }
-  console.log(15, allSets, ans);
-  if (!ans) {
-    return allSets[0].size === numCourses;
+  let status = new Array(numCourses).fill(false);
+  for (let course in allCourses) {
+    dfs(course, allCourses, status, ans, {});
   }
-  return ans;
+  return ans[0];
 };
 
-function union(setA, setB) {
-  var _union = setA;
-  for (var elem of setB) {
-    _union.add(elem);
+function dfs(node, allNodes, status, ans, visited) {
+  if (!visited[node]) {
+    if (!status[node]) {
+      status[node] = true;
+    } else {
+      ans[0] = false;
+      return;
+    }
+    if (allNodes[node] && allNodes[node].length) {
+      for (let newNode of allNodes[node]) {
+        dfs(newNode, allNodes, status, ans, visited);
+      }
+    }
+    status[node] = false;
+    visited[node] = true;
   }
-  return _union;
 }
 
-canFinish(3, [[0, 1], [0, 2], [1, 2]]);
-canFinish(3, [[1, 0], [2, 0], [2, 1]]);
-canFinish(2, [[1, 0], [0, 1]]);
-canFinish(2, [[1, 0]]);
+canFinish(3, [[1, 0], [2, 1]]); // true
+canFinish(2, [[1, 0], [0, 1]]); // false
+canFinish(2, [[1, 0]]); // true
+canFinish(3, [[1, 0], [2, 0], [2, 1]]); // true
+canFinish(3, [[0, 1], [0, 2], [1, 2]]); // true
