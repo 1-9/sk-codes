ff386eeb6f6909137c26053e9dd86696f6226b68
Surya Kotamraju <kotamraju@me.com>
2018-07-12 21:45:41 -0400

    update three sum closest

--- 2018/180704-three-sum-closest.js
+++ 2018/180704-three-sum-closest.js
@@ -1,15 +1,15 @@
 let threeSumClosest = function(nums, target) {
   let minimum = Infinity,
-    ans;
+      ans;
   nums.sort((a, b) => a - b);
 
   for (let i = 0; i < nums.length - 2; i++) {
     let left = i + 1,
-      right = nums.length - 1;
+        right = nums.length - 1;
 
-    while (left >= right) {
+    while (true) {
       let currentSum = calculateSum(nums[i], nums[left], nums[right]),
-        difference = Math.abs(currentSum - target);
+          difference = Math.abs(currentSum - target);
       if (currentSum === target) {
         return target;
       } else if (currentSum < target) {
@@ -17,10 +17,12 @@ let threeSumClosest = function(nums, target) {
       } else if (currentSum > target) {
         right--;
       }
-
       if (difference < minimum) {
         minimum = difference;
         ans = currentSum;
+      }
+      if (left >= right || i >= left) {
+        break;
       }
     }
   }
@@ -28,6 +30,3 @@ let threeSumClosest = function(nums, target) {
 };
 
 const calculateSum = (a, b, c) => a + b + c;
-
-threeSumClosest([-1, 2, 1, -4], 1); // 2
-threeSumClosest([0, 0, 0], 1); // 1
